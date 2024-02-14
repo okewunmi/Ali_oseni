f27dbb9273344ba2a70ae09c96014a82620e7762
okewunmi <okewunmiafeezolaide@gmail.com>
2024-02-14 21:39:28 +0100

    navigate

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+// next.config.js
+
+module.exports = {
+  useFileSystemPublicRoutes: true,
+};
 
 module.exports = nextConfig
 // next.config.js
