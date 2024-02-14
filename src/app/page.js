8e58fa6401c31a4dee07d8acb5e08aa6e926a273
okewunmi <okewunmiafeezolaide@gmail.com>
2024-02-14 21:31:46 +0100

    layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,12 @@
 
 import './globals.css'
 import Home from './index';
+import Layout from './layout'
 export default function App() {
   return (
-    <>
+    <Layout>
         <Home />
-    </>
+    </Layout>
   
   )
 }
