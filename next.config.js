efa6653ee043d483508f0766b79caf0f4404a7a4
okewunmi <okewunmiafeezolaide@gmail.com>
2024-02-26 15:47:45 +0100

    reloading

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental
+: {
+    
+disableOptimizedLoading
+: 
+true
+,
+  },
+}
 
 
+
+module.exports = nextConfig
+// next.config.js
 const withFonts = require('next-fonts');
 
 module.exports = withFonts({
   // Your other Next.js config options here
 });
-// next.config.js
-
-module.exports = {
-  // Disable prefetching in production
-  onDemandEntries: false,
-};
-
-
